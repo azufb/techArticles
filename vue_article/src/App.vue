a07c355bed0ad5b7c6516314f8e848bb702777a4
azufb <curiousazunas@gmail.com>
2021-11-28 20:49:07 +0900

    ヘッダー部分の作成

--- vue_article/src/App.vue
+++ vue_article/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="navBar">
+      <h1>Let's Try Sass!</h1>
+      <div class="naviMenu">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </div>
     <router-view />
   </div>
 </template>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,13 +23,26 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.navBar {
+  padding: 15px 30px;
+  background-color: #2c3e50;
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+
+.naviMenu {
+  position: absolute;
+  right: 30px;
+}
+
+h1 {
+  color: #fff;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #fff;
 }
 
 #nav a.router-link-exact-active {
